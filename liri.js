7538b33a85b6d8737b622cc67281328ab416b55d
Dan Staggs <staggs.dan@gmail.com>
2018-12-09 21:23:26 -0600

    full functionality, very pleased, preparing demo video and readme asap.

--- liri.js
+++ liri.js
@@ -4,73 +4,79 @@ var Spotify = require('node-spotify-api');
 var axios = require("axios");
 var moment = require("moment");
 var inquirer = require("inquirer");
+var fs = require("fs");
 
 var keys = require("./keys");
 // vars to hold the API keys (do I have redundant BIT and OMDB key passing? couldn't get it to work without these.)
-var bandsKey = keys.bands;
-var movieKey = process.env.OMDB_API_KEY
+var bandsKey = process.env.BANDS_API_KEY;
+var movieKey = process.env.OMDB_API_KEY;
 var spotify = new Spotify(keys.spotify);
 
+// bandsintown search functionality
+// dev note - how do I write a console.log to tell the user there are no results?
+function concertSearch() {
+    inquirer.prompt([
+        {
+            name: "concertsearch",
+            message: "What performer's concerts do you want to look up?"
+        }
+    ]).then (function (response){
+        var userTerm = response.concertsearch;
+        var queryURL = "https://rest.bandsintown.com/artists/" + userTerm + "/events?app_id=" + bandsKey;
 
-var userCommand = process.argv[2];
-var userTermEntered = process.argv[3];
+        if (userTerm === "") {
+            console.log("Try searching a musical performer next time.");
+            newSearch();
+        }
 
-// class demonstration of 'switch'
-switch (userCommand) {
-    case "concert-this":
-        console.log("concert");
-        concertSearch();
-        break;
-    case "spotify-this-song":
-        console.log("spotify")
-        songSearch();
-        break;
-    case "movie-this":
-        console.log("movie");
-        movieSearch();
-        break;
-    case "do-what-it-says":
-        console.log("whatever")
-        break;
-    default:
-        console.log("please enter a command")
-        break;
+        else {axios.get(queryURL)
+        .then(function (response){
+            
+            for (let i = 0; i < response.data.length; i++) {
+                var humanTime = moment(response.data[i].datetime).format('L')
+                console.log(`See this artist at ${response.data[i].venue.name} in ${response.data[i].venue.city}, ${response.data[i].venue.region}, on ` + humanTime);
+            }
+            newSearch();
+        })}
+    }
+    )
 }
 
-// bandsintown search functionality
-// dev note - be sure to change artistName to an argv input later
-function concertSearch() {
-    // var userTerm = userTermEntered.replace(/\s/g, "+");
-    var userTerm = "";
-    var queryURL = "https://rest.bandsintown.com/artists/" + userTerm + "/events?app_id=" + bandsKey;
-    console.log(userTerm);
-    
-    axios.get(queryURL)
-    .then(function (response) {
-        var humanTime = moment(response.data[0].datetime).format('LL');
-        
-        console.log("See them on " + humanTime + " at "+ response.data[0].venue.name + " in " + response.data[0].venue.city +", "+ response.data[0].venue.region);
-
-        console.log("See them on " + humanTime + " at "+ response.data[1].venue.name + " in " + response.data[1].venue.city +", "+ response.data[1].venue.region);
-
-        for (let i = 1; i < 4; i++) {
-            console.log("Or see them on " + humanTime + " at "+ response.data[i].venue.name + " in " + response.data[i].venue.city +", "+ response.data[i].venue.region);
-        }
-    })
-    
-};
-
 // spotify search functionality via npmjs.com
+
 function songSearch(){
-    var userTerm = "";
-    
-    spotify.search({ type: 'track', query: 'All the Small Things', limit: 5 }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
+    inquirer.prompt([
+        {
+            name: "search",
+            message: "What song do you want to look up?"
         }
-       
-      console.log(data); 
-      });
+    ]).then(function (response) {
+        var userTerm = response.search;
+
+        if (userTerm === "") {
+            spotify.search({ type: 'track', query: "The+Sign+Ace+of+Base", limit: 1 })
+            .then(function (response) {
+                console.log("You may enjoy this one.");
+                console.log(`Artist: ${response.tracks.items[0].artists[0].name}`);
+                console.log(`Song name: ${response.tracks.items[0].name}`);
+                console.log(`Preview link: ${response.tracks.items[0].preview_url}`);
+                console.log(`Album name: ${response.tracks.items[0].album.name}`);    
+                
+            })
+            newSearch();
+            // console.log("Try searching a song next time.");
+            // newSearch();
+        }
+
+        else {spotify.search({ type: 'track', query: userTerm, limit: 8 })
+            .then(function (response) {
+                console.log(`Artist: ${response.tracks.items[0].artists[0].name}`);
+                console.log(`Song name: ${response.tracks.items[0].name}`);
+                console.log(`Preview link: ${response.tracks.items[0].preview_url}`);
+                console.log(`Album name: ${response.tracks.items[0].album.name}`);    
+                newSearch();
+            })}
+    })
 }
 
 // movie search functionality
@@ -89,15 +95,29 @@ function movieSearch(){
             axios.get(queryURL)
             .then (function(response){
                 console.log("I have it on good authority you should check out this flick");
-                console.log(`Title: ${response.data.Title}, Released: ${response.data.Year}, IMDB Rating: ${response.data.Ratings[0].Value}, Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}, Produced in: ${response.data.Country}, Release language: ${response.data.Language}, Basic plot: ${response.data.Plot}, Starring: ${response.data.Actors}`);
-                newSearch()
+                console.log(`Title: ${response.data.Title}`); 
+                console.log(`Released: ${response.data.Year}`);
+                console.log(`IMDB Rating: ${response.data.Ratings[0].Value}`);
+                console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
+                console.log(`Produced in: ${response.data.Country}`);
+                console.log(`Release language: ${response.data.Language}`);
+                console.log(`Basic plot: ${response.data.Plot}`);
+                console.log(`Starring: ${response.data.Actors}`);
+                newSearch();
             })
         } else {
             var queryURL = "http://www.omdbapi.com/?t=" + userTerm + "&apikey=" + movieKey;
 
             axios.get(queryURL)
             .then (function(response) {
-                console.log(`Title: ${response.data.Title}, Released: ${response.data.Year}, IMDB Rating: ${response.data.Ratings[0].Value}, Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}, Produced in: ${response.data.Country}, Release language: ${response.data.Language}, Basic plot: ${response.data.Plot}, Starring: ${response.data.Actors}`);
+                console.log(`Title: ${response.data.Title}`); 
+                console.log(`Released: ${response.data.Year}`);
+                console.log(`IMDB Rating: ${response.data.Ratings[0].Value}`);
+                console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
+                console.log(`Produced in: ${response.data.Country}`);
+                console.log(`Release language: ${response.data.Language}`);
+                console.log(`Basic plot: ${response.data.Plot}`);
+                console.log(`Starring: ${response.data.Actors}`);
                 newSearch();
             })
             
@@ -105,30 +125,44 @@ function movieSearch(){
     })
 }
 
+// This works and I feel like it shouldn't?
+function fileSystem(){
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        var fileSystemSong = data
+        spotify.search({ type: 'track', query: fileSystemSong, limit: 5 })
+            .then(function (response) {
+                console.log(`Artist: ${response.tracks.items[0].artists[0].name}`);
+                console.log(`Song name: ${response.tracks.items[0].name}`); 
+                console.log(`Preview: ${response.tracks.items[0].preview_url}`); 
+                console.log(`Album name: ${response.tracks.items[0].album.name}`);
+                newSearch();
+            })
+        })
+    }
+                    
 // ask a TA how to make search response reactive to object position instead of direct quotes. tried a few approaches, couldn't get it.
 function primaryFunction() {
     inquirer.prompt([
         {
             type: "list",
             message: "What do you want to search for?",
-            choices: ["Check for a concert", "Look up a song", "Look up a movie", "Random function: pending.", "Exit program"],
+            choices: ["Check for a concert", "Look up a song", "Look up a movie", "Do What It Says (tm)", "Exit program"],
             name: "searchlist",
         }
     ]).then(function(response){
         if (response.searchlist === "Check for a concert") {
-            console.log("concert search works");
             concertSearch();
         } else if (response.searchlist === "Look up a song") {
-            console.log("spotify search works");
             songSearch();
         } else if (response.searchlist === "Look up a movie"){
-            console.log("movie search works");
             movieSearch();
-        } else if (response.searchlist === "Random function: pending."){
-            console.log("this function is pending");
+        } else if (response.searchlist === "Do What It Says (tm)"){
+            fileSystem();            
         } else if (response.searchlist === "Exit program") {
             console.log("Come back any time.");
-            
         }
     })
 }
@@ -144,9 +178,9 @@ function newSearch(){
         if (result.newSearch === true) {
             primaryFunction();
         } else {
-            console.log("Come back any time");
-            
+            console.log("Come back any time");   
         }
     })
 }
+
 primaryFunction();
